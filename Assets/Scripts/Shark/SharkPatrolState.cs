05821770193c9dbefa6baee4f3c48f920a176d88
DarioOtr <otrantodario@gmail.com>
2023-09-30 19:10:19 -0300

    Add Shark Patrol State

--- Assets/Scripts/Shark/SharkPatrolState.cs
+++ Assets/Scripts/Shark/SharkPatrolState.cs
@@ -1,17 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SharkPatrolState : SharkBaseState
 {
+    private float sphereRadius = 40.0f;
+    private float maxDistanceDelta = 1.0f;
+    private Vector3 randomPointInSphere;
+    private Vector3 currentTarget;
+
+    public event Action<Vector3> OnLerpToTarget;
     public SharkPatrolState(string name, State_Machine stateMachine,SharkInputController _sharkController) : base(name, stateMachine,_sharkController) { }
     public override void OnEnter()
     {
+        currentTarget = GetRandomPoint();
         base.OnEnter();
     }
 
     public override void UpdateLogic()
     {
+        float distance = Vector3.Distance(_sharkController.transform.position,currentTarget);
+
+        if (distance >= maxDistanceDelta)
+        {
+            _sharkController.transform.position =
+                Vector3.MoveTowards(_sharkController.transform.position, currentTarget, maxDistanceDelta * Time.deltaTime * _sharkController.GetPatrolSpeed());
+        }
+        else
+        {
+            currentTarget = GetRandomPoint();
+            OnLerpToTarget.Invoke(currentTarget);
+        }
+        
         base.UpdateLogic();
     }
 
@@ -23,6 +45,13 @@ public class SharkPatrolState : SharkBaseState
     private void playAttackAnimation()
     {
         
+    }
+
+    private Vector3 GetRandomPoint()
+    {
+        randomPointInSphere = Random.insideUnitSphere;
+        randomPointInSphere *= sphereRadius;
+        return randomPointInSphere;
     }
 
     public override void AddStateTransitions(string transitionName, State transitionState)
--- Assets/Scripts/Shark/SharkStateMachine.cs
+++ Assets/Scripts/Shark/SharkStateMachine.cs
@@ -2,35 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SharkStateMachine : State_Machine
 {
-    [SerializeField] private SharkInputController sharkController;
+    [FormerlySerializedAs("sharkController")] [SerializeField] private SharkInputController _sharkController;
+    private IEnumerator _lookAtTarget;
     private SharkMoveState _moveState;
     private SharkPatrolState _patrolState;
     private SharkAttackState _attackState;
 
     private void OnEnable()
     {
-        if (sharkController == null)
-            sharkController = GetComponent<SharkInputController>();
-        if (!sharkController)
+        if (_sharkController == null)
+            _sharkController = GetComponent<SharkInputController>();
+        if (!_sharkController)
         {
-            Debug.LogError(message: $"{name}: (logError){nameof(sharkController)} is null");
+            Debug.LogError(message: $"{name}: (logError){nameof(_sharkController)} is null");
             enabled = false;
         }
+        
+        _moveState = new SharkMoveState(nameof(_moveState),this,_sharkController);
+        _patrolState = new SharkPatrolState(nameof(_patrolState),this,_sharkController);
+        _attackState = new SharkAttackState(nameof(_attackState),this,_sharkController);
 
-        sharkController.OnSharkMove += OnSharkMove;
-        sharkController.OnSharkPatrol += OnSharkPatrol;
-        sharkController.OnSharkAttack += OnSharkAttack;
+        _sharkController.OnSharkMove += OnSharkMove;
+        _sharkController.OnSharkPatrol += OnSharkPatrol;
+        _sharkController.OnSharkAttack += OnSharkAttack;
+        _patrolState.OnLerpToTarget += OnPatrolLerpToTarget;
 
-        _moveState = new SharkMoveState(nameof(_moveState),this,sharkController);
-        _patrolState = new SharkPatrolState(nameof(_patrolState),this,sharkController);
-        _attackState = new SharkAttackState(nameof(_attackState),this,sharkController);
         
         base.OnEnable();
     }
-    
+
+    private void OnPatrolLerpToTarget(Vector3 obj)
+    {
+        if (_lookAtTarget != null)
+        {
+            StopCoroutine(_lookAtTarget);
+        }
+
+        _lookAtTarget = LookAtTarget(obj);
+        StartCoroutine(_lookAtTarget);
+    }
+
     private void OnSharkMove()
     {
         SetState(_moveState);
@@ -43,6 +58,20 @@ public class SharkStateMachine : State_Machine
     {
         SetState(_attackState);
     }
+    
+    IEnumerator LookAtTarget(Vector3 currentTarget)
+    {
+        Quaternion initialRot = _sharkController.transform.rotation;
+        Quaternion finalRot = Quaternion.LookRotation(currentTarget - transform.position , Vector3.up);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _sharkController.GetRotationSpeed())
+        {
+            elapsedTime += Time.deltaTime;
+            _sharkController.transform.rotation = Quaternion.Lerp(initialRot,finalRot,elapsedTime);
+            yield return null;
+        }
+    }
 
     protected override State GetInitialState()
     {
@@ -52,8 +81,8 @@ public class SharkStateMachine : State_Machine
     
     private void OnDisable()
     {
-        sharkController.OnSharkMove -= OnSharkMove;
-        sharkController.OnSharkPatrol -= OnSharkPatrol;
-        sharkController.OnSharkAttack -= OnSharkAttack;
+        _sharkController.OnSharkMove -= OnSharkMove;
+        _sharkController.OnSharkPatrol -= OnSharkPatrol;
+        _sharkController.OnSharkAttack -= OnSharkAttack;
     }
 }
