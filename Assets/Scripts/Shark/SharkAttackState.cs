808e0c0a13b3c58eb3a91f3be598ce92aca4bd65
DarioOtr <otrantodario@gmail.com>
2023-09-30 17:09:41 -0300

    Add Shark FSM

---
+++ Assets/Scripts/Shark/SharkAttackState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkAttackState : SharkBaseState
+{
+    public SharkAttackState(string name, State_Machine stateMachine,SharkInputController _sharkController) : base(name, stateMachine,_sharkController) { }
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+
+    public override void UpdateLogic()
+    {
+        base.UpdateLogic();
+    }
+
+    public override void UpdatePhysics()
+    {
+        base.UpdatePhysics();
+    }
+    
+    private void playAttackAnimation()
+    {
+        
+    }
+
+    public override void AddStateTransitions(string transitionName, State transitionState)
+    {
+        base.AddStateTransitions(transitionName, transitionState);
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+}
