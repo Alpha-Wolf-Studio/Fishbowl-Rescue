d77752786416f7eef8b6f077d5af32348d61f1f8
Sauron <federicoolive@gmail.com>
2023-10-01 19:18:51 -0300

    Update RandomVisibleParts.cs

--- Assets/Scripts/Objects/RandomVisibleParts.cs
+++ Assets/Scripts/Objects/RandomVisibleParts.cs
@@ -1,18 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomVisibleParts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<GameObject> listParts = new List<GameObject>();
+    [SerializeField] private int minParts = 3;
+    [SerializeField] private int maxParts = 6;
+
+    private void Start ()
     {
-        
+        if (listParts.Count > maxParts)
+            maxParts = listParts.Count;
+
+        for (int i = 0; i < listParts.Count; i++)
+            listParts[i].SetActive(false);
+
+        int random = Random.Range(minParts, maxParts);
+
+        for (int i = 0; i < random; i++)
+        {
+            FindPart().SetActive(true);
+        }
+
+        listParts[2].transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+        listParts[3].transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+        listParts[4].transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject FindPart ()
     {
-        
+        int counter = 50;
+        int random = 0;
+        bool wasFound = false;
+        while (!wasFound)
+        {
+            counter--;
+            if (counter < 0)
+            {
+                Debug.Log("Error");
+                wasFound = true;
+            }
+            random = Random.Range(0, listParts.Count);
+            if (!listParts[random].activeSelf)
+                wasFound = true;
+        }
+
+        return listParts[random];
     }
 }
