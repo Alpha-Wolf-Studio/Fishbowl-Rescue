d55e72aad67c1d334c09eeed00f33ba181bab91a
Rojoin <nachoarrastua@gmail.com>
2023-10-02 19:09:27 -0300

    Add WaterLevel texture

---
+++ Assets/Scripts/Entities/WaterTexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+    public class WaterTexture : MonoBehaviour
+    {
+        private MeshRenderer rend;
+        [Range(0,1f)]
+        [SerializeField] private float speed =0.5f;
+
+        void Start()
+        {
+            rend = GetComponent<MeshRenderer>();
+        }
+
+        void Update()
+        {
+            float scaleX = Mathf.Cos(Time.time) * speed + 1;
+            float scaleY = Mathf.Sin(Time.time) * speed + 1;
+            rend.material.mainTextureScale = new Vector2(scaleX, scaleY);
+        }
+    }
+}
